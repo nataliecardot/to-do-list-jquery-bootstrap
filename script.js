0fcd63f4854ff9415472ed515661f1b729c244b8
Natalie Cardot <33387780+nataliecardot@users.noreply.github.com>
2018-04-07 00:20:04 -0700

    Update styling

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 $(function() {
 
-  let degree = 45;
+  let degree = 90;
 
-  init();
-
-  function init() {
-    // Sets CSS transform properties
-    $('.fa-plus').css({
+  // Rotates '+' and drops down or recloses input box on click
+  $('.fa-plus').on('click', function() {
+    if (degree === 135) {
+      degree = 45;
+    }
+    // Sets CSS transform properties. Selector 'this' refers to '+' (with class '.fa-plus') being clicked
+    $(this).css({
       '-webkit-transform': 'rotate(' + degree + 'deg)',
       '-moz-transform': 'rotate(' + degree + 'deg)',
       '-ms-transform': 'rotate(' + degree + 'deg)',
@@ -15,9 +17,11 @@ $(function() {
       'transition': '0.2s linear'
     });
     degree += 45;
-  }
+    // Animates opacity of text box, setting its display style property to none once its opacity reaches 0 if it's visible, or the inverse if already hidden
+    $('input[type="text"]').fadeToggle();
+  });
 
-  // Checks off specific items upon click and creates class 'completed'
+  // Checks off specific items upon click and adds them to class 'completed'
   $('ul').on('click', 'li', function() {
     $(this).toggleClass('completed');
   });
@@ -45,25 +49,6 @@ $(function() {
       // Creates new li from input and adds to beginning of ul
       $('ul').prepend('<li><span><i class="fa fa-trash"></i></span> ' + itemText + '</li>');
     }
-  });
-
-  // Rotates '+' and drops down or recloses input box on click
-  $('.fa-plus').on('click', function() {
-    if (degree === 135) {
-      degree = 45;
-    }
-    // Sets CSS transform properties. Selector 'this' refers to '+' (with class '.fa-plus') being clicked
-    $(this).css({
-      '-webkit-transform': 'rotate(' + degree + 'deg)',
-      '-moz-transform': 'rotate(' + degree + 'deg)',
-      '-ms-transform': 'rotate(' + degree + 'deg)',
-      '-o-transform': 'rotate(' + degree + 'deg)',
-      'transform': 'rotate(' + degree + 'deg)',
-      'transition': '0.2s linear'
-    });
-    degree += 45;
-    // Animates opacity of text box, setting its display style property to none once its opacity reaches 0 if it's visible, or the inverse if already hidden
-    $('input[type="text"]').fadeToggle();
   });
 
 });
